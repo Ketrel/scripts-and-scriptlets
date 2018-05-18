541d5d4c8767946276fe0b3d3b6ab33512696606
Ketrel <webmaster@krahs-emag.com>
2018-05-18 16:50:26 -0400

    Initial Commit

---
+++ php/filelisting.php
@@ -0,0 +1,35 @@
+<?php
+    $indent=function($t="",$n=1,$ts="    "){
+        return str_repeat($ts,$n).$t;
+    };
+    $metadata=[];
+    if (file_exists("./metadata.info")){
+        $metadata = parse_ini_file("./metadata.info",true);
+    }
+    $output="";    
+    $title=isset($metadata['Metadata']['title']) ? $metadata['Metadata']['title'] : "File Listing";
+    $heading=isset($metadata['Metadata']['heading']) ? $metadata['Metadata']['heading'] : "File Listing";
+
+    $dir=array_values(array_diff(preg_grep('/^\.+.?+/',scandir('.'),PREG_GREP_INVERT),[basename($_SERVER['PHP_SELF']),'metadata.info']));
+    if (count($dir)>=1){
+        $output.=$indent("<ul>\n",0);
+        foreach ($dir as $val){
+            $output.=$indent("<li><a href=\"./${val}\">",3);
+            $output.=(isset($metadata['Files'][$val]) ? $metadata['Files'][$val] : ${val});
+            $output.="</a></li>\n";
+        }
+        $output.=$indent("</ul>\n",2);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?=$title;?></title>
+</head>
+<body>
+    <div class="wrapper">
+        <h2><?=$heading;?></h2>
+        <?=$output;?>
+    </div>
+</body>
+</html>
