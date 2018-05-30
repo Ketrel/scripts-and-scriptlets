bb61681fde772baf5819401594d54cdddf3411b5
Ketrel <webmaster@krahs-emag.com>
2018-05-30 14:00:18 -0400

    Continuing work on setup script

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ configDimensions
 
 # Generate Menu
 exec 3>&1
-menuSelection=$(\
+menuSelection=$(DIALOGRC="${scriptdir}/support_files/dialogMsgBox" \
 dialog \
     --backtitle "Setup" \
     --clear \
@@ -63,17 +63,14 @@ dialog \
     "1" "Copy Scripts To:  \$HOME/Scripts/<file(s)>" \
     "2" "Copy dotfiles To: \$HOME/<file(s)>" \
     "3" "Copy dotfiles (Except .profile) To: \$HOME/<file(s)>" \
-    "4" "(Currently Unused Option, Does Nothing)" \
+    "4" "Copy specific dotfiles To: \$HOME/<file(s)>" \
     "5" "(Currently Unused Option, Does Nothing)" \
     2>&1 1>&3)
 ${tputBin} clear
-# if [ ! -z "${menuSelection}" ]; then
-#     echo "Selected: ${menuSelection}"
-# else
-#     echo "Canceled or Escaped Out of Menu"
-# fi
 case ${menuSelection} in
     1)
+        
+
         printf "$(${tputBin} setaf 2)%b$(${tputBin} sgr0)\n" \
             "Copying included scripts to \"${HOME}/Scripts\""
         echo "Dry Run: Not Doing Anything for Real"
@@ -95,18 +92,51 @@ case ${menuSelection} in
         find ${scriptdir}/dotfiles -type f ! -name ".profile" -exec echo cp -t \"${HOME}/\" {} +
     ;; 
     4)
-        results=$(dialog --backtitle "Test" --checklist "Select Test" \
-            ${mainHeight} \
-            ${mainWidth} \
-            ${menuHeight} \
-            "1" "Item 1" "off" \
-            "2" "Item 2" "off" \
-            "3" "Item 3" "on" \
-            "4" "Item 4" "off" \
-            2>&1 1>&3)
+        cd ${scriptdir}/dotfiles
+        fileList=$(find . -type f -exec sh -c 'printf "%s\n" "$(basename {})"' \;)
+        cd - 1>/dev/null
+        i=0
+        checkList=''
+        while read -r line
+        do
+            i=$(($i+1))
+            checkList="${checkList}\"${i}\" \"${line}\" \"off\" "
+        done <<EOF
+${fileList}
+EOF
+            results=$(eval "DIALOGRC="${scriptdir}/support_files/dialogMsgBox" dialog --backtitle 'Setup' \
+                --title 'File Select' \
+                --checklist 'Select DotFiles' \
+                ${mainHeight} \
+                ${mainWidth} \
+                ${menuHeight} \
+                ${checkList} \
+                2>&1 1>&3")
         clear
-        echo "--${results}--"
+        if [ ! -z "${results}" ]; then
+            chosen=$(echo "${results}" | sed -e 's/ /\n/g')
+            i=1
+            infoMsg=''
+            while read -r line 
+            do
+                if grep -E "\\b${i}\\b" >/dev/null <<EOF
+${results}
+EOF
+                then
+                    infoMsg="${infoMsg}Copied "${line}" to $HOME\n"
+                    infoMsg="${infoMsg}  DRY RUN: cp \"${scriptdir}/dotfiles/${line}\" \"\$HOME/${line}\"\n"
+                fi
+                i=$(($i+1))
+            done <<EOFF
+${fileList}
+EOFF
+        else
+            printf "$($tputBin setaf 1)%b$(${tputBin} sgr0)\n" "Error: No Files Selected"
+            exit 1
+        fi
     ;;
 esac
-
+if [ ! -z "${infoMsg}" ]; then
+    DIALOGRC="${scriptdir}/support_files/dialogMsgBox" dialog  --backtitle "Setup Results" --title "Results" --msgbox "$infoMsg" ${mainHeight} ${mainWidth}
+fi
 exec 3>&-
