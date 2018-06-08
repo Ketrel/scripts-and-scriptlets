91b1f8b717e8f15fe35abf943d413948d6109d30
Ketrel <webmaster@krahs-emag.com>
2018-06-08 10:12:55 -0400

    Updated QD Listing script

--- php/qd-filelisting.php
+++ php/qd-filelisting.php
@@ -1,16 +1,26 @@
 <?php
-    /* * * * * * * * * * * * * * * * * * * * * *
-    *
-    *  QD File Listing
-    *   -Special Files
-    *   --metadata.info
-    *   ---[Metadata]
-    *   ----title="Title Tag Contents"
-    *   ----heading="H2 Tag Contents"
-    *   ---[Files]
-    *   ----filename="Listing Name Override"
-    *
-    * * * * * * * * * * * * * * * * * * * * * */
+    // * * * * * * * * * * * * * * * * * * * * *
+    //
+    // QD File Listing
+    //  -Special Files
+    //  --metadata.info
+    //  ---[Metadata]
+    //  ----title="Title Tag Contents"
+    //  ----heading="H2 Tag Contents"
+    //  ---[Files]
+    //  ----filename="Listing Name Override"
+    //
+    // * * * * * * * * * * * * * * * * * * * * *
+
+    // * * * * * * * * * * * * * * * * * * * * *
+    //
+    // Config Options
+    //
+    // Include Directories In Listing?
+       $conf['dirs']=TRUE;    
+    //
+    //* * * * * * * * * * * * * * * * * * * * * * 
+
 
     $indent=function($t="",$n=1,$ts="    "){
         return str_repeat($ts,$n).$t;
@@ -26,13 +36,34 @@
 
     // ^\.+.?+ = match files that start with .
     // PREG_GREP_INVERT = and give me everything that doesn't match
-    $dir=preg_grep('/^\.+.?+/',scandir('.'),PREG_GREP_INVERT);
+    $predir=preg_grep('/^\.+.?+/',scandir('.'),PREG_GREP_INVERT);
 
     // Remove this script (with any name) and any file named 'metadata.info' from the listing
-    $dir=array_diff($dir,[basename($_SERVER['PHP_SELF']),'metadata.info']);
-    
+    $predir=array_diff($dir,[basename($_SERVER['PHP_SELF']),'metadata.info']);
+   
+
+    //Break apart into separate file and directory arrays 
+    $dir_d = [];
+    $dir_f = [];
+    foreach($predir as $val){
+        if (is_file($val)){
+            $dir_f[] = $val;
+        }
+        else{
+            $dir_d[] = $val;
+        }
+    }
+
+    //Assemble final array based on directory setting
+    if($conf['dirs']){
+        $dir = array_merge($dir_d,$dir_f);
+    }
+    else{
+        $dir = array_values($dir_f);
+    }
+ 
     // Reindex the array. Not strictly needed, but meh.
-    $dir=array_values($dir);
+    //$dir=array_values($dir);
 
 
     if (count($dir)>=1){
@@ -52,7 +83,7 @@
         <ul>
 <?php
     foreach ($filelist as $key=>$val){
-        echo $indent("<li><a href=\"${key}\">${val}</a></li>\n",3);
+        echo $indent("<li><a href=\"${key}\">${val}".(is_dir($val) ? "/" : "")."</a></li>\n",3);
     }
 ?>
         </ul>
