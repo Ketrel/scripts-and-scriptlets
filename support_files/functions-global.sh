806122c35f0bdd3f515766e92efab58117319ce5
Ketrel <undisclosed@undisclosed.com>
2018-06-12 12:38:02 -0400

    Further work on setup

--- support_files/functions-global.sh
+++ support_files/functions-global.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+
+checkBinary()
+{
+    if [ "$(command -v "${1}")" ]; then
+        return 0        
+    else
+        printf "${cRed}%b${cReset}\\n" "Error: Specifcied Binary '${1}' Not Found." 
+        if [ "${2}" = "optional" ]; then
+            printf "${cRed}%b${cReset}\\n\\n" "----\\nWhile the script will work without it, functionality may be limited." 
+        else
+            printf "${cRed}%b${cReset}\\n\\n" "----\\nThis binary is required for this script to function.\\n If the binary is present on our machine,\\n please ensure it's accessible via your PATH." 
+        fi
+        return 1
+    fi
+}
+
+configDimensions()
+{
+    # 80x25 is the safe assumption
+    mainWidth=$(( $(tput cols 2>/dev/null || printf '70' ) * 8 / 10 ))
+    mainHeight=$(( $(tput lines 2>/dev/null || printf '15' ) * 8 / 10 ))
+    menuHeight=$(( mainHeight - 8 ))
+}
+
 dirPick(){
     _fpickDir=''
     _fprevDir=''
@@ -28,9 +52,9 @@ dirPick(){
         _flisting=$(find ./ -maxdepth 1 -type d ! -name "." ! -name '*"*' -exec printf '"%s" OFF\n' '{}' \; | sed -e 's/\.\///g' | LC_ALL=C sort -g | tr '\n' ' ' )
 
         if [ "${tuiBin}" = "dialog" ]; then
-            _fpickDir=$(eval "DIALOGRC=${mainRC} dialog --no-items --radiolist \"Choose A Directory: ${_fstartDir}\" 24 80 16 '<This Directory>' ON '..' OFF ${_flisting}  2>&1 1>&3")
+            _fpickDir=$(eval "DIALOGRC=${mainRC} dialog --no-items --radiolist \"Choose A Directory: ${_fstartDir}\" ${mainHeight} ${mainWidth} ${menuHeight} '<This Directory>' ON '..' OFF ${_flisting}  2>&1 1>&3")
         else
-            _fpickDir=$(eval "NEWT_COLORS_FILE=\"${mainRC}\" whiptail --noitem --radiolist \"Choose A Directory: ${_fstartDir}\" 24 80 16 '<This Directory>' ON '..' OFF ${_flisting}  2>&1 1>&3")
+            _fpickDir=$(eval "NEWT_COLORS_FILE=\"${mainRC}\" whiptail --noitem --radiolist \"Choose A Directory: ${_fstartDir}\" ${mainHeight} ${mainWidth} ${menuHeight} '<This Directory>' ON '..' OFF ${_flisting}  2>&1 1>&3")
         fi
         _fret="${?}"
 
