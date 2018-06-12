b726e422cd768811154bd3a8fc0c88cada4a526a
Ketrel <undisclosed@undisclosed.com>
2018-06-12 15:52:23 -0400

    Restructuring setup

--- support_files/functions-whiptail.sh
+++ support_files/functions-whiptail.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-msgBox()
-{
+msgBox(){
     if [ -n "${1}" ]; then
         NEWT_COLORS_FILE="${mainRC}" whiptail --msgbox "${1}" ${mainHeight} ${mainWidth}
     fi
 }
 
-yesnoBox()
-{
+yesnoBox(){
     if [ -n "${1}" ]; then
         NEWT_COLORS_FILE="${mainRC}" whiptail --yesno "${1}" ${mainHeight} ${mainWidth}
         result=$?
@@ -16,8 +14,183 @@ yesnoBox()
     fi
 }
 
-generateSelect()
-{
+copyFiles(){
+    if [ -z "${1}" ] || [ -z "${2}" ]; then
+       printf 'Fatal Program Error: Misuse of function\nExiting\n' 
+       exit 1
+    fi
+    infoMsg="${infoMsg}Copying included scripts to \"${2}\""
+    infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
+
+    # if [ -z "${3}" ] && [ "${3}" = "noprofile" ]; then
+        # find "${1}" -type f ! -name ".profile" -exec cp {} "${2}/" \;
+    # else
+        # find "${1}" -type f -exec cp {} "${2}/" \;
+    # fi
+}
+
+main(){
+ exit 7
+}
+
+mainMenu(){
+    menuSelection="0"
+    while [ "${menuSelection}" = "0" ] || [ "${menuSelection}" = "-" ] || [ "${menuSelection}" = "?" ]; do
+        if ! menuSelection=$(NEWT_COLORS_FILE="${mainRC}" \
+            whiptail \
+            --backtitle "Setup" \
+            --clear \
+            --menu \
+                "Automated Setup Options" \
+                ${mainHeight} \
+                ${mainWidth} \
+                ${menuHeight} \
+                "1" "Copy Scripts To Scripts Dir" \
+                "2" "Copy Specific Scripts To Scripts Dir" \
+                "-" "" \
+                "3" "Copy Dotfiles To Dotfiles Dir" \
+                "4" "Copy Dotfiles (Except .profile) Dotfiles Dir" \
+                "5" "Copy Specific Dotfiles To Dotfiles Dir" \
+                "-" "" \
+                "0" "Configuration Options" \
+                "?" "Show Current Config" \
+                "-" "" \
+                "Q" "Quit" \
+            2>&1 1>&3); then exit 0; fi
+    done
+
+    case "${menuSelection}" in
+        '1')
+            copyFiles "${scriptsDir}" "${scriptsDestDir}"
+            ;;
+        '2')
+            infoMsg=''
+            copySelectedFiles "${scriptsDir}" "${scriptsDestDir}"
+            if [ ${?} -eq 1 ]; then
+                return 0
+            fi
+            ;;
+        '3')
+            copyFiles "${dotfilesDir}" "${dotfilesDestDir}"
+            ;;
+        '4')
+            copyFiles "${dotfilesDir}" "${dotfilesDestDir}" "noprofile"
+            ;;
+        '5')
+            infoMsg=''
+            copySelectedFiles "${dotfilesDir}" "${dotfilesDestDir}"
+            if [ ${?} -eq 1 ]; then
+                return 0
+            fi
+            ;;
+        '?')
+            msgBox "Current Config\\n----------\\n\\nDestination for scripts:\\n  ${scriptsDestDir}\\n\\nDestination for dotfiles:\\n  ${dotfilesDestDir}"
+            ;;
+        '0')
+            ;;
+        'Q')
+            printf '\033c'
+            exit 0
+            ;;
+    esac
+    if [ -n "${infoMsg}" ]; then
+        NEWT_COLORS_FILE="${mainRC}" whiptail  --backtitle "Setup" --title "Results" --msgbox "$infoMsg" ${mainHeight} ${mainWidth}
+        infoMsg=''
+    fi
+
+    if NEWT_COLORS_FILE="${mainRC}" whiptail --backtitle "Setup" --title "Continue" --yesno "Finished\\nRun More Tasks?" ${mainHeight} ${mainWidth}
+    then
+        infoMsg=''
+        return 0
+    else
+        return 1
+    fi
+}
+
+options(){
+    tempDir=''
+    # infoMsg=''
+
+    menuConfigSelect="x"
+    while [ -n "${menuConfigSelect}" ] && [ ! "${menuConfigSelect}" = "0" ]; do
+        menuConfigSelect=$(NEWT_COLORS_FILE="${mainRC}" \
+            whiptail \
+            --backtitle "Setup" \
+            --clear \
+            --menu \
+                "Configuration Options" \
+                ${mainHeight} \
+                ${mainWidth} \
+                ${menuHeight} \
+            "1" "Set Scripts Destination Directory" \
+            "2" "Set Dotfiles Destination Directory" \
+            "-" "" \
+            "0" "Return" \
+            "?" "Show Current Config" \
+            "C" "Create Missing Directories (Not Implimented)" \
+            "-" "" \
+            "Q" "Quit" \
+            2>&1 1>&3)
+
+        case "${menuConfigSelect}" in
+            '1')
+                chosenDir=''
+                if [ -d "${scriptsDestDir}" ] && [ -r "${scriptsDestDir}" ]; then
+                    dirPick "${scriptsDestDir}" 'Choose a scripts directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
+                    dirPick "${HOME}" 'Choose a scripts directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                else 
+                    dirPick "/" 'Choose a scripts directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                fi
+                if [ "${dirRet}" -eq 1 ]; then
+                    msgBox "Canceled\\nScripts Directory Unchanged\\nCurrent Value: ${scriptsDestDir}"
+                else
+                    msgBox "Changing Scripts Destination Directory\\nFrom: ${scriptsDestDir}\\nTo: ${chosenDir}"
+                    scriptsDestDir="${chosenDir}"
+                    chosenDir=''
+                fi
+                ;;
+            '2')
+                chosenDir=''
+                if [ -d "${dotfilesDestDir}" ] && [ -r "${dotfilesDestDir}" ]; then
+                    dirPick "${dotfilesDestDir}" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
+                    dirPick "${HOME}" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                else 
+                    dirPick "/" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                    dirRet=${?}
+                fi
+                if [ "${dirRet}" -eq 1 ]; then
+                    msgBox "Canceled\\ndotfiles Directory Unchanged\\nCurrent Value: ${dotfilesDestDir}"
+                else
+                    msgBox "Changing dotfiles Destination Directory\\nFrom: ${dotfilesDestDir}\\nTo: ${chosenDir}"
+                    dotfilesDestDir="${chosenDir}"
+                    chosenDir=''
+                fi
+                ;;
+            '?')
+                msgBox "Current Config\\n----------\\n\\nDestination for scripts:\\n  ${scriptsDestDir}\\n\\nDestination for dotfiles:\\n  ${dotfilesDestDir}"
+                ;;
+            'C')
+                if yesnoBox "Create The Following Directories?\\n(If They Don't Exist)\\n\\n${dotfilesDestDir}\\n${scriptsDestDir}"; then
+                    msgBox "Would Create Directories\\nBut Not Yet Implimented"
+                fi
+                ;;
+            'Q')
+                printf '\033c'
+                exit 0
+                ;;
+        esac
+    done
+    return 0
+}
+
+copySelectedFiles(){
     fileList=''
     checkList=''
     results=''
@@ -75,173 +248,5 @@ EOFF
         msgBox "Error: No Files Selected"
         exit 70
     fi
-}
-
-# Generate Menu
-main()
-{
-    menuSelection=''
-    menuConfigSelect=''
-    tempDir=''
-    # infoMsg=''
-
-    menuSelection="0"
-    while [ "${menuSelection}" = "0" ] || [ "${menuSelection}" = "-" ] || [ "${menuSelection}" = "?" ]; do
-        if ! menuSelection=$(NEWT_COLORS_FILE="${mainRC}" \
-            whiptail \
-            --backtitle "Setup" \
-            --clear \
-            --menu \
-                "Automated Setup Options" \
-                ${mainHeight} \
-                ${mainWidth} \
-                ${menuHeight} \
-                "1" "Copy Scripts To Scripts Dir" \
-                "2" "Copy Specific Scripts To Scripts Dir" \
-                "-" "" \
-                "3" "Copy Dotfiles To Dotfiles Dir" \
-                "4" "Copy Dotfiles (Except .profile) Dotfiles Dir" \
-                "5" "Copy Specific Dotfiles To Dotfiles Dir" \
-                "-" "" \
-                "0" "Configuration Options" \
-                "?" "Show Current Config" \
-                "-" "" \
-                "Q" "Quit" \
-            2>&1 1>&3); then exit 0; fi
-            #In the menu below\n  \$HOME refers to \"${HOME}\"" \
-
-            case "$menuSelection" in
-                '?')
-                    msgBox "Current Config\\n----------\\n\\nDestination for scripts:\\n  ${scriptsDestDir}\\n\\nDestination for dotfiles:\\n  ${dotfilesDestDir}"
-                ;;
-                '0')
-                    menuConfigSelect="x"
-                    while [ -n "${menuConfigSelect}" ] && [ ! "${menuConfigSelect}" = "0" ]; do
-                        menuConfigSelect=$(NEWT_COLORS_FILE="${mainRC}" \
-                            whiptail \
-                            --backtitle "Setup" \
-                            --clear \
-                            --menu \
-                                "Configuration Options" \
-                                ${mainHeight} \
-                                ${mainWidth} \
-                                ${menuHeight} \
-                            "1" "Set Scripts Destination Directory" \
-                            "2" "Set Dotfiles Destination Directory" \
-                            "-" "" \
-                            "0" "Return" \
-                            "?" "Show Current Config" \
-                            "C" "Create Missing Directories (Not Implimented)" \
-                            "-" "" \
-                            "Q" "Quit" \
-                            2>&1 1>&3)
-
-                        case "${menuConfigSelect}" in
-                            '1')
-                                chosenDir=''
-                                if [ -d "${scriptsDestDir}" ] && [ -r "${scriptsDestDir}" ]; then
-                                    dirPick "${scriptsDestDir}" 'Choose a scripts directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
-                                    dirPick "${HOME}" 'Choose a scripts directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                else 
-                                    dirPick "/" 'Choose a scripts directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                fi
-                                if [ "${dirRet}" -eq 1 ]; then
-                                    msgBox "Canceled\\nScripts Directory Unchanged\\nCurrent Value: ${scriptsDestDir}"
-                                else
-                                    msgBox "Changing Scripts Destination Directory\\nFrom: ${scriptsDestDir}\\nTo: ${chosenDir}"
-                                    scriptsDestDir="${chosenDir}"
-                                    chosenDir=''
-                                fi
-                            ;;
-                            '2')
-                                chosenDir=''
-                                if [ -d "${dotfilesDestDir}" ] && [ -r "${dotfilesDestDir}" ]; then
-                                    dirPick "${dotfilesDestDir}" 'Choose a dotfiles directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
-                                    dirPick "${HOME}" 'Choose a dotfiles directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                else 
-                                    dirPick "/" 'Choose a dotfiles directory.\nCurrently Viewing: '
-                                    dirRet=${?}
-                                fi
-                                if [ "${dirRet}" -eq 1 ]; then
-                                    msgBox "Canceled\\ndotfiles Directory Unchanged\\nCurrent Value: ${dotfilesDestDir}"
-                                else
-                                    msgBox "Changing dotfiles Destination Directory\\nFrom: ${dotfilesDestDir}\\nTo: ${chosenDir}"
-                                    dotfilesDestDir="${chosenDir}"
-                                    chosenDir=''
-                                fi
-                            ;;
-                            '?')
-                                msgBox "Current Config\\n----------\\n\\nDestination for scripts:\\n  ${scriptsDestDir}\\n\\nDestination for dotfiles:\\n  ${dotfilesDestDir}"
-                            ;;
-                            'C')
-                                if yesnoBox "Create The Following Directories?\\n(If They Don't Exist)\\n\\n${dotfilesDestDir}\\n${scriptsDestDir}"; then
-                                    msgBox "Would Create Directories"
-                                fi
-                            ;;
-                            'Q')
-                                printf '\033c'
-                                exit 0
-                            ;;
-                        esac
-                    done
-                ;;
-                'Q')
-                    printf '\033c'
-                    exit 0
-                ;;
-            esac
-    done
-    case "${menuSelection}" in
-        '1')
-            infoMsg="${infoMsg}Copying included scripts to \"${scriptsDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
-
-            # find "${scriptsDir}" -type f -exec cp {} "${scriptsDestDir}/" \;
-        ;;
-        '2')
-            infoMsg=''
-            generateSelect "${scriptsDir}" "${scriptsDestDir}"
-            if [ ${?} -eq 1 ]; then
-                return 0
-            fi
-        ;;
-        '3')
-            infoMsg="${infoMsg}Copying dotfiles to: \"${dotfilesDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
-
-            # find "${scriptsDir}" -type f -exec cp {} "${dotfilesDestDir}/" \;
-        ;;
-        '4')
-            infoMsg="${infoMsg}Copying dotfiles (excuding .profile) to: \"${dotfilesDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
-
-            # find "${dotfilesDir}" -type f ! -name ".profile" -exec cp {} "${dotfilesDestDir}/" \;
-        ;;
-        '5')
-            infoMsg=''
-            generateSelect "${dotfilesDir}" "${dotfilesDestDir}"
-            if [ ${?} -eq 1 ]; then
-                return 0
-            fi
-        ;;
-    esac
-    if [ -n "${infoMsg}" ]; then
-        NEWT_COLORS_FILE="${mainRC}" whiptail  --backtitle "Setup" --title "Results" --msgbox "$infoMsg" ${mainHeight} ${mainWidth}
-        infoMsg=''
-    fi
-
-    if NEWT_COLORS_FILE="${mainRC}" whiptail --backtitle "Setup" --title "Continue" --yesno "Finished\\nRun More Tasks?" ${mainHeight} ${mainWidth}
-    then
-        infoMsg=''
-        return 0
-    else
-        return 1
-    fi
+    return 0
 }
