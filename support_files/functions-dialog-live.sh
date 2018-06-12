aa613c0d9db887aa460a8ca9df83cd8198edb885
Ketrel <webmaster@krahs-emag.com>
2018-06-12 14:11:28 -0400

    Yet more work on setup script

--- support_files/functions-dialog-live.sh
+++ support_files/functions-dialog-live.sh
@@ -1,43 +1,12 @@
 #!/bin/sh
 
-scriptdir=$(dirname "${0}")
-supportdir="${scriptdir}/support_files"
-scriptsDir="${scriptdir}/shell"
-dotfilesDir="${scriptdir}/dotfiles"
-scriptsDestDir="${HOME}/Scripts"
-dotfilesDestDir="${HOME}"
-dialogRC="${supportdir}/mainRC-dialog"
-newtColorsRC="${supportdir}/mainRC-whiptail"
-mainRC="${dialogRC}"
-infoMsg=''
-
-checkBinary()
-{
-    if [ "$(command -v "${1}")" ]; then
-        return 0        
-    else
-        printf "${cRed}%b${cReset}\\n" "Error: Specifcied Binary '${1}' Not Found." 
-        if [ "${2}" = "optional" ]; then
-            printf "${cRed}%b${cReset}\\n\\n" "----\\nWhile the script will work without it, functionality may be limited." 
-        else
-            printf "${cRed}%b${cReset}\\n\\n" "----\\nThis binary is required for this script to function.\\n If the binary is present on our machine,\\n please ensure it's accessible via your PATH." 
-        fi
-        return 1
-    fi
-}
-configDimensions()
-{
-    # 80x25 is the safe assumption
-    mainWidth=$(( $(tput cols 2>/dev/null || printf '70' ) * 8 / 10 ))
-    mainHeight=$(( $(tput lines 2>/dev/null || printf '15' ) * 8 / 10 ))
-    menuHeight=$(( mainHeight - 3 ))
-}
 msgBox()
 {
     if [ -n "${1}" ]; then
         DIALOGRC="${mainRC}" dialog --msgbox "${1}" ${mainHeight} ${mainWidth}
     fi
 }
+
 yesnoBox()
 {
     if [ -n "${1}" ]; then
@@ -46,15 +15,24 @@ yesnoBox()
         return ${result}
     fi
 }
+
 generateSelect()
 {
+    fileList=''
+    checkList=''
+    results=''
+    chosen=''
+    # infoMsg=''
+
     if [ -z "${1}" ] || [ -z "${2}" ]; then
         printf "%b\\n" "Unrecoverable Script Failure\\nNeeds Debugging"
         exit 5
     fi
+
     cd "${1}" || exit 1
     fileList=$(find . -type f -exec sh -c 'printf "%s\n" "$(basename "${1}")"' sh {} \; | sort)
     cd - 1>/dev/null || exit 1
+
     i=0
     checkList=''
     while read -r line
@@ -76,79 +54,59 @@ EOF
     if [ -n "${results}" ]; then
         chosen=$(echo "${results}" | sed -e 's/ /\n/g')
         i=1
-            
+
         # Blank out infoMsg for fresh use
         infoMsg=''
-        while read -r line 
+        while read -r line
         do
             if grep -E "\\b${i}\\b" >/dev/null <<EOF
 ${chosen}
 EOF
             then
                 infoMsg="${infoMsg}Copied \"${line}\" to ${2}\\n"
-                infoMsg="${infoMsg}  DRY RUN: cp \"${1}/${line}\" \"${2}/${line}\"\\n"
-                # cp "${1}/${line}" "${2}/${line}" || { printf "${cRed}%b${cReset}\\n" "Error Copying Files"; exit 41; }
+                cp "${1}/${line}" "${2}/${line}" || { printf "${cRed}%b${cReset}\\n" "Error Copying Files"; exit 41; }
             fi
             i=$((i+1))
         done <<EOFF
 ${fileList}
 EOFF
     else
-        DIALOGRC="${mainRC}" dialog --msgbox "Error: No Files Selected" ${mainHeight} ${mainWidth}
+        msgBox "Error: No Files Selected"
         exit 70
     fi
 }
 
-# Make sure 'which' exists before proceding
-# which which || exit 44
-# not needed if using command -v
-
-# Set up some colors
-cRed=$(tput setaf 1 2>/dev/null || printf '' )
-cReset=$(tput sgr0 2>/dev/null || printf '' )
-cBold=$(tput bold 2>/dev/null || printf '' )
-
-# Ensure dialog is present
-if ! checkBinary "dialog" ; then
-    echo "Major Failure"
-    exit 2
-fi 
-
-# Setup Dimensions
-configDimensions
-
-# Ensure infoMsg is empty at this point
-infoMsg=''
-
-# Create fd 3
-exec 3>&1
-
 # Generate Menu
 main()
 {
+    menuSelection=''
+    menuConfigSelect=''
+    tempDir=''
+    # infoMsg=''
+
     menuSelection="0"
     while [ "${menuSelection}" = "0" ] || [ "${menuSelection}" = "-" ] || [ "${menuSelection}" = "?" ]; do
-        menuSelection=$(DIALOGRC="${mainRC}" \
+        if ! menuSelection=$(DIALOGRC="${mainRC}" \
             dialog \
             --backtitle "Setup" \
             --clear \
             --menu \
-                "Automated Setup Options\\n---\\n" \
+                "Automated Setup Options" \
                 ${mainHeight} \
                 ${mainWidth} \
                 ${menuHeight} \
                 "1" "Copy Scripts To Scripts Dir" \
                 "2" "Copy Specific Scripts To Scripts Dir" \
-                "-" "----------" \
+                "-" "" \
                 "3" "Copy Dotfiles To Dotfiles Dir" \
                 "4" "Copy Dotfiles (Except .profile) Dotfiles Dir" \
                 "5" "Copy Specific Dotfiles To Dotfiles Dir" \
-                "-" "----------" \
+                "-" "" \
                 "0" "Configuration Options" \
                 "?" "Show Current Config" \
-                "-" "----------" \
+                "-" "" \
                 "Q" "Quit" \
-            2>&1 1>&3)
+            2>&1 1>&3); then exit 0; fi
             #In the menu below\n  \$HOME refers to \"${HOME}\"" \
 
             case "$menuSelection" in
@@ -169,43 +127,53 @@ main()
                                 ${menuHeight} \
                             "1" "Set Scripts Destination Directory" \
                             "2" "Set Dotfiles Destination Directory" \
-                            "-" "----------" \
+                            "-" "" \
                             "0" "Return" \
                             "?" "Show Current Config" \
                             "C" "Create Missing Directories (Not Implimented)" \
-                            "-" "----------" \
+                            "-" "" \
                             "Q" "Quit" \
                             2>&1 1>&3)
 
                         case "${menuConfigSelect}" in
                             '1')
-                                tempDir=$(DIALOGRC=${mainRC} dialog \
-                                    --backtitle "Setup" \
-                                    --clear \
-                                    --dselect \
-                                        "${HOME}/" \
-                                        ${mainHeight} \
-                                        ${mainWidth} \
-                                    2>&1 1>&3)
-                                # && [ -d ${tempDir} ]
-                                if [ -n "${tempDir}" ] && [ ! "${tempDir}" = "/" ]; then
-                                    msgBox "Scripts Destination Changed.\\n Was: ${scriptsDestDir}\\n Is Now: ${tempDir}"
-                                    scriptsDestDir=${tempDir%/}
+                                chosenDir=''
+                                if [ -d "${scriptsDestDir}" ] && [ -r "${scriptsDestDir}" ]; then
+                                    dirPick "${scriptsDestDir}" 'Choose a scripts directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
+                                    dirPick "${HOME}" 'Choose a scripts directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                else 
+                                    dirPick "/" 'Choose a scripts directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                fi
+                                if [ "${dirRet}" -eq 1 ]; then
+                                    msgBox "Canceled\\nScripts Directory Unchanged\\nCurrent Value: ${scriptsDestDir}"
+                                else
+                                    msgBox "Changing Scripts Destination Directory\\nFrom: ${scriptsDestDir}\\nTo: ${chosenDir}"
+                                    scriptsDestDir="${chosenDir}"
+                                    chosenDir=''
                                 fi
                             ;;
                             '2')
-                                tempDir=$(DIALOGRC=${mainRC} dialog \
-                                    --backtitle "Setup" \
-                                    --clear \
-                                    --dselect \
-                                        "${HOME}/" \
-                                        ${mainHeight} \
-                                        ${mainWidth} \
-                                    2>&1 1>&3)
-                                # && [ -d ${tempDir} ]
-                                if [ -n "${tempDir}" ] && [ ! "${tempDir}" = "/" ]; then
-                                    msgBox "Dotfiles Destination Changed.\\n Was: ${dotfilesDestDir}\\n Is Now: ${tempDir}"
-                                    dotfilesDestDir=${tempDir%/}
+                                chosenDir=''
+                                if [ -d "${dotfilesDestDir}" ] && [ -r "${dotfilesDestDir}" ]; then
+                                    dirPick "${dotfilesDestDir}" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                elif [ -d "${HOME}" ] && [ -r "${HOME}" ]; then
+                                    dirPick "${HOME}" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                else 
+                                    dirPick "/" 'Choose a dotfiles directory.\nCurrently Viewing: '
+                                    dirRet=${?}
+                                fi
+                                if [ "${dirRet}" -eq 1 ]; then
+                                    msgBox "Canceled\\ndotfiles Directory Unchanged\\nCurrent Value: ${dotfilesDestDir}"
+                                else
+                                    msgBox "Changing dotfiles Destination Directory\\nFrom: ${dotfilesDestDir}\\nTo: ${chosenDir}"
+                                    dotfilesDestDir="${chosenDir}"
+                                    chosenDir=''
                                 fi
                             ;;
                             '?')
@@ -214,7 +182,7 @@ main()
                             'C')
                                 if yesnoBox "Create The Following Directories?\\n(If They Don't Exist)\\n\\n${dotfilesDestDir}\\n${scriptsDestDir}"; then
                                     msgBox "Would Create Directories"
-                                fi 
+                                fi
                             ;;
                             'Q')
                                 printf '\033c'
@@ -232,41 +200,44 @@ main()
     case "${menuSelection}" in
         '1')
             infoMsg="${infoMsg}Copying included scripts to \"${scriptsDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
-            
-            # find "${scriptsDir}" -type f -exec cp {} "${scriptsDestDir}/" \;
+
+            find "${scriptsDir}" -type f -exec cp {} "${scriptsDestDir}/" \;
         ;;
         '2')
             infoMsg=''
             generateSelect "${scriptsDir}" "${scriptsDestDir}"
+            if [ ${?} -eq 1 ]; then
+                return 0
+            fi
         ;;
         '3')
             infoMsg="${infoMsg}Copying dotfiles to: \"${dotfilesDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
 
-            # find "${scriptsDir}" -type f -exec cp {} "${dotfilesDestDir}/" \;
-        ;; 
+            find "${scriptsDir}" -type f -exec cp {} "${dotfilesDestDir}/" \;
+        ;;
         '4')
             infoMsg="${infoMsg}Copying dotfiles (excuding .profile) to: \"${dotfilesDestDir}\""
-            infoMsg="${infoMsg}\\n    Dry Run: Not Doing Anything for Real"
-            
-            # find "${dotfilesDir}" -type f ! -name ".profile" -exec cp {} "${dotfilesDestDir}/" \;
+
+            find "${dotfilesDir}" -type f ! -name ".profile" -exec cp {} "${dotfilesDestDir}/" \;
         ;;
         '5')
             infoMsg=''
             generateSelect "${dotfilesDir}" "${dotfilesDestDir}"
-        ;; 
+            if [ ${?} -eq 1 ]; then
+                return 0
+            fi
+        ;;
     esac
     if [ -n "${infoMsg}" ]; then
         DIALOGRC="${mainRC}" dialog  --backtitle "Setup" --title "Results" --msgbox "$infoMsg" ${mainHeight} ${mainWidth}
+        infoMsg=''
     fi
-    #if [ "${?}" -eq 0 ]; then
+
     if DIALOGRC="${mainRC}" dialog --backtitle "Setup" --title "Continue" --yesno "Finished\\nRun More Tasks?" 7 ${mainWidth}
     then
         infoMsg=''
-        main
+        return 0
+    else
+        return 1
     fi
-    printf '\033c'
 }
-main
-exec 3>&-
