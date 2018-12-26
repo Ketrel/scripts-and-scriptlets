3e6919a68ef64bc8086d14365b9f3a84871563ce
Ketrel <undisclosed@undisclosed.com>
2018-12-26 12:44:26 -0500

    Replaced sed with awk, since I'm already using it for the date filter, may as well do it in one go

--- shell/wlfilter.sh
+++ shell/wlfilter.sh
@@ -23,15 +23,18 @@ esac
 if [ -n "${1}" ] && [ -f "${1}" ]; then
     if [ -n "${2}" ]; then
         if [ -n "${3}" ]; then
-            out="$(awk -v startdate="${2}" -v enddate="${3}" '$1>=startdate && $1<=enddate {print $0}' "${1}")"
+            awk \
+                -v startdate="${2}" \
+                -v enddate="${3}" \
+                '$3 !~ "<?-->?" && $1>=startdate && $1<=enddate {print $0}' "${1}"
         else
-            out="$(awk -v startdate="${2}" '$1>=startdate {print $0}' "${1}")"
+            awk \
+                -v startdate="${2}" \
+                '$3 !~ "<?-->?" && $1>=startdate {print $0}' "${1}"
         fi
-        out="$(printf '%s' "${out}" | sed -e '/^[0-9-]\+ [0-9:]\+\s\+<\?-->\?/d')"
     else
-        out="$(sed -e '/^[0-9-]\+ [0-9:]\+\s\+<\?-->\?/d' "${1}")"
+        awk '$3 !~ "<?-->?" { print $0 }' "${1}"
     fi
-    printf '%s\n' "${out}"
 else
     echo "File Not Specified or Specified File Does Not Exist"
 fi
