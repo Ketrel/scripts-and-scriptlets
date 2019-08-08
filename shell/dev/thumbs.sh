e64c321f1b1d23a14aae0d6d9a0f9d00c639004d
Ketrel <undisclosed@undisclosed.com>
2019-08-08 17:15:13 -0400

    Work on thumbs.sh

--- shell/dev/thumbs.sh
+++ shell/dev/thumbs.sh
@@ -3,24 +3,39 @@
 if [ ! -f "${1}" ]; then
     printf "Error: File Not Specified or Does Not Exist\n"
     exit 1
+else
+    vidName="${1%%.*}"
 fi
 
-frameCount=$(\
-    ffmpeg \
-    -hide_banner \
-    -i "${1}" \
-    -map 0:v:0 \
-    -c copy \
-    -f null \
-    - 2>&1 \
-        | grep \
-            -oe 'frame=[0-9]\+' \
-        | cut \
-            -d '=' \
-            -f 2)
+#frameCount=$(\
+#    ffmpeg \
+#        -hide_banner \
+#        -i "${1}" \
+#        -map 0:v:0 \
+#        -c copy \
+#        -f null \
+#        - 2>&1 \
+#            | grep \
+#                -oe 'frame=[0-9]\+' \
+#            | cut \
+#                -d '=' \
+#                -f 2)
 
-ffmpeg -i "${1}" -vf fps=1/60 "./thumb_test/img%03d.jpg"
-            
-#printf -- "---%d---\n" ${frameCount}
-
-ffprobe -i 'Hustle S01E01.avi' -v error -show_format | grep duration | cut -d '=' -f 2
+if [ ! -d "./${vidName}" ]; then
+    mkdir "${vidName}"
+fi
+vidLength=$(\
+    ffprobe \
+        -i "${1}" \
+        -v error \
+        -show_format\
+            | grep 'duration' \
+            | cut -d '=' -f 2 \
+            | xargs printf '%.0f\n')
+ffmpeg -i "${1}" -vf fps=15/${vidLength}  "./${vidName}/img%03d.jpg"
+cd "${vidName}"
+montage img*.jpg -tile '4x' -geometry '360x>' "${vidName}_thumbs.jpg"
+mv "${vidName}_thumbs.jpg" ..
+rm *.jpg
+cd ..
+rmdir "${vidName}"
